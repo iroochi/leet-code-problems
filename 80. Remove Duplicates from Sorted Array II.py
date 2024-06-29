c29e982e09838e9e4995bbf58f2743671fe736ec
Roochita Ikkurthy <120745349+iroochi@users.noreply.github.com>
2024-06-29 17:23:47 -0400

    remove_duplicates_II

--- 80. Remove Duplicates from Sorted Array II.py
+++ 80. Remove Duplicates from Sorted Array II.py
@@ -1,15 +1,12 @@
-def remove_duplicates(nums):
-    if len(nums) == 0:
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) == 0:
             return 0
-    # keeping track of the position where the next unique or duplicate element will be placed
-    j = 2 
-    nums_size = len(nums)
-    # loop starts with index 2 because the first 2 elements are always allowed because each unique element appears at most twice
-    for i in range(2, nums_size): 
-        if nums[i] != nums[j-2]:
-            nums[j] = nums[i]
-            j += 1
-    return j
-    return nums[:j]
-    
+        j = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[j-2]:
+                nums[j] = nums[i]
+                j += 1
+        return j
+        return nums[:j]
 
