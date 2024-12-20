d8ebdc6e451638a080b24f1d1aae164131f320a2
Roochita Ikkurthy <120745349+iroochi@users.noreply.github.com>
2024-12-19 23:05:50 -0500

    LRU

---
+++ 146. LRU Cache.py
@@ -0,0 +1,28 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {} # to store key -> value
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        val = self.cache.pop(key)
+        self.cache[key] = val
+        return val
+    
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+        else:
+            if len(self.cache) == self.capacity:
+                del self.cache[next(iter(self.cache))]
+        self.cache[key] = value
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
