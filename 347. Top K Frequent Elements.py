2372d81e2a89217eef677827c6cf2db0afb7e020
Roochita Ikkurthy <120745349+iroochi@users.noreply.github.com>
2024-12-20 01:58:18 -0500

    Add files via upload

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -1,12 +1,13 @@
-from collections import Counter
+from collections import Counter # counts the occurrences of elements in an iterable
 import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = Counter(nums)
+        freq = Counter(nums) 
+        # example: nums = [1, 1, 1, 2, 2, 3] ; output: Counter({1: 3, 2: 2, 3: 1})
 
         heap = []
         for num, fq in freq.items():
-            heapq.heappush(heap, (fq, num))
+            heapq.heappush(heap, (fq, num)) # [(1, 3), (3, 1), (2, 2)]
             if len(heap) > k:
                 heapq.heappop(heap)
 
